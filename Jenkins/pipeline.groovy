d11a385623bff8659239592bdb1021e80cf78d2c
abderrezak boukazouha <abderrezak.bkz@gmail.com>
2024-11-29 18:31:11 +0100

    modify pipline script

--- Jenkins/pipeline.groovy
+++ Jenkins/pipeline.groovy
@@ -14,7 +14,7 @@ pipeline {
 
         stage('Checkout Code') {
             steps {
-                git branch: 'main', url: 'https://your-repository-url.git'
+                git branch: 'main', url: 'https://github.com/abderrezakBoukazouha/gatling-poc'
             }
         }
 
