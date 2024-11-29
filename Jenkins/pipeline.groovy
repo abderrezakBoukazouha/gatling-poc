10eef4b7c9fe7ea4b942eee9886e18dd3139b961
abderrezak boukazouha <abderrezak.bkz@gmail.com>
2024-11-29 18:03:16 +0100

    adding a groovy script to build the App

---
+++ Jenkins/pipeline.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+
+    environment {
+        TIMESTAMP = "${new Date().format('yyyyMMddHHmmss')}"
+    }
+
+    stages {
+        stage('Clean Workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+
+        stage('Checkout Code') {
+            steps {
+                git branch: 'main', url: 'https://your-repository-url.git'
+            }
+        }
+
+        stage('Build and Test Docker Image') {
+            steps {
+                // Build and test Docker image
+                script {
+                    def imageName = "presentation-app:${env.TIMESTAMP}"
+                    sh "docker build -t ${imageName} ."
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            echo 'Pipeline execution complete.'
+        }
+        success {
+            echo 'Pipeline executed successfully.'
+        }
+        failure {
+            echo 'Pipeline failed.'
+        }
+    }
+}
